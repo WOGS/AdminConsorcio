84d81b3a718a9dd614b90af72c75c1a33f1689bc
martinmen <martinhmsuni@gmail.com>
2020-12-01 15:30:08 -0300

    Se agrega implementacion correcta de Vista de Error Corercciones de UI

---
+++ Consorcio/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Consorcio.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(int error = 0)
+        {
+            switch (error)
+            {
+                case 404:
+                    ViewBag.Title = "Ocurrio un error inesperado";
+                    ViewBag.Description = "Esto es muy vergonzoso, esperemos que no vuelva a pasar ..";
+                    break;
+
+                case 500:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "La URL que está intentando ingresar no existe";
+                    break;
+
+                default:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "Algo salio muy mal :( ..";
+                    break;
+            }
+
+            return View("~/views/error/_ErrorPage.cshtml");
+        }
+    }
+}
--- Consorcio/Global.asax.cs
+++ Consorcio/Global.asax.cs
@@ -14,5 +14,20 @@ namespace Consorcio
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+
+            int error = httpException != null ? httpException.GetHttpCode() : 0;
+
+            Server.ClearError();
+            Response.Redirect("~/Error/index/" + error.ToString());
+            //Response.Redirect(String.Format("../ErrorPage/?error={0}", error, exception.Message));
+            //  Response.Redirect(String.Format("~/Error/?error={0}", error, exception.Message));
+        }
     }
 }
